f93d8d533917d89c423081a1f6cabfad10179f05
dsharmaESF <48754018+dsharmaESF@users.noreply.github.com>
2020-10-05 20:33:03 +0100

    Add files via upload

---
+++ ApiSampleCode.cs
@@ -0,0 +1,122 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Net;
+
+namespace codesample
+{
+    public class ApiSampleCode
+    {
+        private static readonly string SubscriptionKey = "<Enter the Subscription Key here>";
+        private static readonly string BaseUrl = "<Enter the Base Url here, for e.g. https://oat-api-customerengagement.platform.education.gov.uk>";
+        private static string Refresh_token { get; set; }
+        private RestClient sampleClient;
+        private RestRequest sampleRequest;
+       
+
+        public ApiSampleCode()
+        {
+            sampleClient = new RestClient()
+            {
+                BaseUrl = new System.Uri(BaseUrl)
+            };
+        }
+
+        public void GetOpenInfo()
+        {
+            // construct the GET request for our DfE Open Information endpoint 
+            sampleRequest = new RestRequest()
+            {
+               Resource = "DfE/open-info",
+               Method = Method.GET
+            };
+
+            // Add Subscription Key Header
+            sampleRequest.AddHeader("Ocp-Apim-Subscription-Key", SubscriptionKey);
+
+            // Execute the query and retrieve results
+            var queryResult = sampleClient.Execute(sampleRequest);
+            HttpStatusCode statusCode = queryResult.StatusCode;
+            int numericStatusCode = (int)statusCode;
+        }
+
+        public void GetApplicationInfo()
+        {
+            // construct the GET request for our DfE Application Information endpoint 
+            sampleRequest = new RestRequest()
+            {
+                Resource = "DfE/application-info",
+                Method = Method.GET
+            };
+
+            // Add Headers
+            sampleRequest.AddHeader("Ocp-Apim-Subscription-Key", SubscriptionKey);
+            sampleRequest.AddHeader("authorisation", "Bearer " + GetAccessToken());
+            sampleRequest.AddHeader("accept", "application/json; charset=utf-8");
+
+            // Execute the query and retrieve results
+            var response = sampleClient.Execute(sampleRequest);
+        }
+
+        public void GetUserInfo()
+        {
+            // construct the GET request for our DfE Application Information endpoint 
+            sampleRequest = new RestRequest()
+            {
+                Resource = "DfE/user-info",
+                Method = Method.GET
+            };  
+
+            // Add Headers
+            sampleRequest.AddHeader("Ocp-Apim-Subscription-Key", SubscriptionKey);
+            sampleRequest.AddHeader("authorisation", "Bearer " + GetAccessToken());
+            sampleRequest.AddHeader("accept", "application/json; charset=utf-8");
+
+            // Execute the query and retrieve results
+            var response = sampleClient.Execute(sampleRequest);
+        }
+
+        private string GetAccessToken()
+        {
+            var tokenClient = new RestClient();
+            var tokenRequest = new RestRequest()
+            {
+                Resource = "<token endpoint resource>",
+                Method = Method.POST,
+            };
+            tokenRequest.AddParameter("grant_type", "client_credentials");
+            tokenRequest.AddParameter("client_id", "<PASTE client id here>");
+            tokenRequest.AddParameter("client_secret", "<PASTE client secret here>");
+            tokenRequest.AddParameter("Content-Type", "application/x-www-form-urlencoded");
+
+            // make call to get token
+            IRestResponse response = tokenClient.Execute(tokenRequest);
+
+            // extract access token and refresh token
+            Refresh_token = JObject.Parse(response.Content).SelectToken("$..refresh_token").ToString();
+            return JObject.Parse(response.Content).SelectToken("$..access_token").ToString();
+        }
+
+        private string RefreshAccessToken()
+        {
+            var tokenClient = new RestClient();
+            var tokenRequest = new RestRequest()
+            {
+                Resource = "<token endpoint resource>",
+                Method = Method.POST,
+            };
+            tokenRequest.AddParameter("grant_type", "refresh_token");
+            tokenRequest.AddParameter("refresh_token", Refresh_token);
+            tokenRequest.AddParameter("client_id", "<PASTE client id here>");
+            tokenRequest.AddParameter("client_secret", "<PASTE client secret here>");
+            tokenRequest.AddParameter("Content-Type", "application/x-www-form-urlencoded");
+
+            // make call to get token
+            IRestResponse response = tokenClient.Execute(tokenRequest);
+
+            // extract token
+            return JObject.Parse(response.Content).SelectToken("$..access_token").ToString();
+        }
+    }
+}
+
+
